e14ea16ffc32fafa948f26b7c8120ee353d4a290
yousrahmed <yousrahmed46@yahoo.com>
2018-12-12 15:22:20 +0200

    pjocet

---
+++ dbo.Craditcarrds.sql
@@ -0,0 +1,10 @@
+﻿CREATE TABLE [dbo].[Craditcarrds] (
+    [ID]         INT            IDENTITY (1, 1) NOT NULL,
+    [C_name]     NVARCHAR (14)  NULL,
+    [C_address]  NVARCHAR (MAX) NULL,
+    [C_Number]   INT            NOT NULL,
+    [Vcs]        INT            NULL,
+    [Expirydate] NVARCHAR (MAX) NULL,
+    CONSTRAINT [PK_Craditcarrds] PRIMARY KEY CLUSTERED ([ID] ASC)
+);
+
